0815d6e500dfcbe0ce43e4a65b1f3deeceb4eb41
ducmint864 <nddminh2021@gmail.com>
2024-03-21 20:40:44 +0700

    Correct env variable names and error messages.

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,24 @@
 import express, { Request, Response } from 'express';
 import cookieParser from 'cookie-parser';
 import authRouter from './routes/auth'
+import 'dotenv/config';
 
 // Check environement
-if (!process.env.JWT_SECRET) {
+if (!process.env.ACCESS_TOKEN_SECRET) {
     console.error('Env error: access token secret not configured');
     process.exit(1);
 };
-
-if (!process.env.POSTGRES_CONNECTION_STRING) {
-    console.error('Env error: refresh token secret not configured');
-    process.exit(1);
-}
 
 if (!process.env.REFRESH_TOKEN_SECRET) {
     console.error('Env error: refresh token secret not configured');
     process.exit(1);
 }
+
+if (!process.env.DB_CONNECTION_STRING) {
+    console.error('Env error: database connection string not configured');
+    process.exit(1);
+}
+
 
 const port = 3000;
 const root = '/'
