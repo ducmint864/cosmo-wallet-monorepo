37b7f63fd21d4ab1bb931a281c207e9d8322b826
ducmint864 <nddminh2021@gmail.com>
2024-03-26 22:49:38 +0700

    Add new test route

--- src/routes/test.ts
+++ src/routes/test.ts
@@ -1,10 +1,11 @@
-import express from 'express';
-import { userAuth } from '../middlewares/user-auth';
-import { retrieveEncryptedMnemonic  } from '../controllers/mnemonic';
-
+import express from "express";
+import { userAuth } from "../middlewares/user-auth";
+import { retrieveEncryptedMnemonic  } from "../controllers/mnemonic";
+import { deriveNewAccount } from "../controllers/auth";
 
 const testRouter = express.Router();
 
-testRouter.route('/mnemonic').post(userAuth, retrieveEncryptedMnemonic);
+testRouter.route("/mnemonic").post(userAuth, retrieveEncryptedMnemonic);
+testRouter.route("/deriveNewAcc").post(userAuth, deriveNewAccount);
 
 export default testRouter;
