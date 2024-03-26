a664fa0ef9e7e415723b457cd06b9e23f6d03827
ducmint864 <nddminh2021@gmail.com>
2024-03-26 22:48:04 +0700

    Resolve merge conflit with main branch in routes/auth.ts

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import express from 'express';
 import { register, login, retrieveNewToken } from '../controllers/auth';
 
@@ -7,16 +6,5 @@ const authRouter = express.Router();
 authRouter.route('/register').post(register);
 authRouter.route('/login').post(login);
 authRouter.route('/retrieveNewToken').get(retrieveNewToken);
-=======
-import express from "express";
-import { register, login } from "../controllers/auth";
-import { userAuth } from "../middlewares/user-auth";
-
-const authRouter = express.Router();
-
-authRouter.route("/register").post(register);
-authRouter.route("/login").post(login);
-// authRouter.route('/update').put(adminAuth, update);
->>>>>>> main
 
 export default authRouter;
