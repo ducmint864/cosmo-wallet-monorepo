0c156809bf73f0d622106644fdd8f8ede1266374
ducmint864 <nddminh2021@gmail.com>
2024-04-06 22:55:08 +0700

    Change up error handling style in user-auth middleware to be consistent with other controller files

--- src/middlewares/user-auth.ts
+++ src/middlewares/user-auth.ts
@@ -1,34 +1,36 @@
 import "dotenv/config";
 import { NextFunction, Request, Response } from "express";
+import { errorHandler } from "./errors/error-handler";
 import { decodeAndVerifyToken } from "../helpers/jwt-helper";
 import { prisma } from "../database/prisma";
 import createError from "http-errors";
-import config  from "../config";
+import config from "../config";
 
 export const userAuth = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
-	const token = req.cookies.accessToken;
-	if (token) {
-		const decoded = decodeAndVerifyToken(token, config.auth.accessToken.secret);
-		if (decoded instanceof Error) {
-			const err = createError(400, "Unauthorized access token");
-			next(err);
-		} else {
-			if (!(prisma.base_account.findFirst({
-				where: {
-					OR: [
-						{ email: decoded.email },
-						{ username: decoded.username }
-					]
+	try {
+		const token = req.cookies.accessToken;
+		if (token) {
+			const decoded = decodeAndVerifyToken(token, config.auth.accessToken.secret);
+			if (decoded instanceof Error) {
+				throw createError(400, "Unauthorized access token");
+			} else {
+				if (!(prisma.base_account.findFirst({
+					where: {
+						OR: [
+							{ email: decoded.email },
+							{ username: decoded.username }
+						]
 				}
-			}))) {
-				throw createError(401, "Unknown identity");
+				}))) {
+					throw createError(401, "Unknown identity");
+				}
+				next();
 			}
-
-			next();
+		} else {
+			throw createError(400, "Missing jwt");
 		}
-	} else {
-		const err = createError(400, "Missing jwt");
-		next(err);
+	} catch (err) {
+		errorHandler(err, req, res, next);
 	}
 };
 
