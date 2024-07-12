89d201debf4f18bea8679bb361140abeaa23ddcd
ducmint864 <nddminh2021@gmail.com>
2024-07-12 11:58:17 +0700

    Improve error handler function

--- src/auth-module/middlewares/errors/error-handler.ts
+++ src/auth-module/middlewares/errors/error-handler.ts
@@ -1,28 +1,36 @@
 import { Request, Response, NextFunction } from "express";
 import { HttpError } from "http-errors";
 
-export function errorHandler(err: HttpError, req: Request, res: Response, next: NextFunction) {
+function getErrResponse(
+	statusCode: number,
+	errMsg: string,
+	stackTrace: string | null | undefined
+): object {
+	return {
+		status: statusCode,
+		message: errMsg,
+		stack: stackTrace || ""
+	};
+}
+
+function errorHandler(err: HttpError, req: Request, res: Response, next: NextFunction) {
 	if (res.headersSent) {
 		return next(err);
 	}
 
 	// Handle internal server errors in detais
 	if (err.message.includes("Unique constraint failed on the fields: (`email`)")) {
-		return res.status(409).json({
-			message: "Email has already been used",
-			stack: err.stack
-		});
+		return res.status(409)
+		.json(getErrResponse(err.statusCode, "Email has been taken", err.stack));
 	}
 
 	if (err.message.includes("Unique constraint failed on the fields: (`username`)")) {
-		return res.status(409).json({
-			message: "Username has already been used",
-			stack: err.stack
-		});
+		return res.status(409)
+		.json(getErrResponse(err.statusCode, "Username has been taken", err.stack));
 	}
 
-	return res.status(err.statusCode || 500).json({
-		message: err.message,
-		stack: err.stack,
-	});
+	return res.status(err.statusCode || 500)
+	.json(getErrResponse(err.statusCode || 500, err.message, err.stack));
 }
+
+export { errorHandler };
