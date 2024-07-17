27736852df29507a7547403a2e1244d4e9ef2088
ducmint864 <nddminh2021@gmail.com>
2024-07-17 21:46:36 +0700

    Add find wallet function

--- src/query-module/controllers/wallet-account.ts
+++ src/query-module/controllers/wallet-account.ts
@@ -3,7 +3,7 @@ import { prisma } from "../../database/prisma";
 import { WalletAccountDTO } from "thasa-wallet-interface";
 import { UserAccountJwtPayload } from "../../auth-module/helpers/types/BaseAccountJwtPayload";
 import { errorHandler } from "../../auth-module/middlewares/errors/error-handler";
-import { pick, mapKeys, camelCase } from "lodash";
+import { pick, mapKeys, camelCase, chain } from "lodash";
 import createHttpError from "http-errors";
 
 // Note -> Get all wallet of the user
@@ -66,4 +66,63 @@ async function getMyWalletAccountInfo(req: Request, res: Response, next: NextFun
 	}
 }
 
-export { getMyWalletAccountInfo };
+
+async function findWithAddress(req: Request, res: Response, next: NextFunction): Promise<void> {
+	const {
+		address: _address,
+	} = req.query;
+
+	try {
+		if (!_address) {
+			throw createHttpError(400, "Please specify wallet address");
+		}
+
+		const accessToken = <UserAccountJwtPayload>req.body.decodedAccessTokenPayload;  // token guaranteed to be valid, decoded by user-auth middleware
+		const userAccountID: number = accessToken.userAccountId;
+
+		const walletAccount = await prisma.wallet_accounts.findUnique({
+			where: {
+				address: _address.toString().toLowerCase(),
+			},
+			select: {
+				address: true,
+				wallet_account_id: true,
+				user_account_id: true,
+				nickname: true,
+			}
+		})
+
+		if (!walletAccount) {
+			throw createHttpError(400, "Wallet account not found");
+		}
+
+		const transformedObj = chain(walletAccount)
+			.mapValues((value, key) => {
+				if (key === "nickname" || key === "user_account_id") {
+					const ownedByUser: boolean = walletAccount.user_account_id === userAccountID;
+					if (!ownedByUser) {
+						value = undefined; // Set nickname, wallet-account-id to undefined if wallet doesn't belong to user
+					}
+				}
+				return value;
+			})
+			.mapKeys((_, key) => {
+				return camelCase(key);
+			})
+			.value();
+
+		const walletAccountDTO = <WalletAccountDTO>pick(
+			transformedObj,
+			["walletAccountId", "userAccountId", "walletOrder", "address", "nickname", "cryptoHdPath"]
+		);
+
+
+		// Success
+		res.status(200).json(walletAccountDTO);
+
+	} catch (err) {
+		errorHandler(err, req, res, next);
+	}
+}
+
+export { getMyWalletAccountInfo, findWithAddress };
--- src/query-module/routes/wallet-account.ts
+++ src/query-module/routes/wallet-account.ts
@@ -1,9 +1,10 @@
 import express, { Router } from "express";
 import { requireAccessToken } from "../../auth-module/middlewares/user-auth";
-import { getMyWalletAccountInfo  } from "../controllers/wallet-account";
+import { getMyWalletAccountInfo, findWithAddress } from "../controllers/wallet-account";
 
 const walletAccountQueryRouter: Router = express.Router();
 
 walletAccountQueryRouter.route("/my-wallet").get(requireAccessToken, getMyWalletAccountInfo );
+walletAccountQueryRouter.route("/find").get(requireAccessToken, findWithAddress);
 
 export { walletAccountQueryRouter };
