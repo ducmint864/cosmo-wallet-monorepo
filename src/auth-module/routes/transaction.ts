28bfff4b149bb0a5aa7b41f887effa0e02421f7b
ducmint864 <nddminh2021@gmail.com>
2024-07-23 00:56:02 +0700

    Delete obsolete files

--- src/auth-module/routes/transaction.ts
+++
@@ -1,9 +0,0 @@
-// import express from 'express';
-// import { userAuth } from "../middlewares/user-auth"
-// import { sendCoin } from '../controllers/transaction';
-
-// const transactionRouter = express.Router();
-
-// transactionRouter.route("/sendCoin").post(userAuth, sendCoin);
-
-// export default transactionRouter;
