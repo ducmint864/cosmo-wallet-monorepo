85099d5f734215890a61186525e34f5c19d1df34
ducmint864 <nddminh2021@gmail.com>
2024-07-23 00:51:57 +0700

    Export prisma client in barrel file

--- src/connections/database/prisma.ts
+++ src/connections/database/prisma.ts
@@ -1,4 +1,9 @@
 import { PrismaClient } from "@prisma/client";
 
 
+/**
+ * Because prisma implements connection pooling under-the-hood,
+ * there's no real benefit to building a connection manager class
+ * rather than briefly exporting an instance of PrismaClient
+ * */ 
 export const prisma = new PrismaClient();
--- src/connections/index.ts
+++ src/connections/index.ts
@@ -1,4 +1,5 @@
 import { WebSocketClientManager } from "./websocket/WebSocketClientManager";
+import { prisma } from "./database/prisma";
 
 // Singleton instance of WebSocketClientManager
 const wsUrl = "ws://localhost:26657/websocket";
@@ -8,4 +9,5 @@ const webSocketClientManager = WebSocketClientManager.getInstance(wsUrl);
 
 export {
 	webSocketClientManager,
-}
+	prisma,
+};
