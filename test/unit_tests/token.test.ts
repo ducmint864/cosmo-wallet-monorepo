9ca845ca205af296555e204707fab3642528e898
Agony Thorn <traminhtrong2604@gmail.com>
2024-07-31 22:36:03 +0700

    style: command for token test

--- test/unit_tests/token.test.ts
+++ test/unit_tests/token.test.ts
@@ -10,26 +10,32 @@ describe('decodeAndVerifyToken', () => {
     });
 
     it('should return the payload if the token is valid', () => {
+        // Arrange
         const token = "valid-token";
         const payload: UserAccountJwtPayload = {userAccountId: 123};
-
+        
         (jwt.verify as jest.Mock).mockReturnValue(payload);
 
+        // Act
         const result = decodeAndVerifyToken(token, publicKey);
 
+        // Assert
         expect(jwt.verify).toHaveBeenCalledWith(token, publicKey);
         expect(result).toEqual(payload);
     })
 
     it('should null if the token invalid', () => {
+        // Arrange
         const token = "invalid-token";
         
         (jwt.verify as jest.Mock).mockImplementation(() => {
             throw new Error("Invalid token");
           });
-
+        
+        // Act
         const result = decodeAndVerifyToken(token, publicKey);
 
+        // Assert
         expect(jwt.verify).toHaveBeenCalledWith(token, publicKey);
         expect(result).toBeNull();
     })
