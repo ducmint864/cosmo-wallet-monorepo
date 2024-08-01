5fd0a596de3685677edc906ae85f78c26373abf3
ducmint864 <nddminh2021@gmail.com>
2024-08-01 14:57:52 +0700

    Use cometHttpManager as comet-bft node provider to mitigate hard-coding URL

--- src/transaction-module/controllers/bank.ts
+++ src/transaction-module/controllers/bank.ts
@@ -8,7 +8,7 @@ import { decrypt, getEncryptionKey, getSigner } from "../../general/helpers/cryp
 import { getStringsFromRequestBody, getObjectFromRequestBody } from "../../general/helpers/request-parser";
 import { Coin } from "thasa-wallet-interface";
 import { writeFile } from "fs"
-import { cometWsManager } from "../../connections";
+import { cometWsManager, cometHttpManager } from "../../connections";
 import WebSocket from "ws";
 import createHttpError from "http-errors";
 
@@ -94,9 +94,9 @@ async function sendCoin(
 		const accounts = await signer.getAccounts();
 		console.log(accounts);
 
-		const url = "http://localhost:26657";
+		const cometHttpUrl: string = await cometHttpManager.getNode();
 		const client: SigningStargateClient = await SigningStargateClient.connectWithSigner(
-			url,
+			cometHttpUrl,
 			signer,
 			{ gasPrice: GasPrice.fromString("0.001stake") }
 		);
