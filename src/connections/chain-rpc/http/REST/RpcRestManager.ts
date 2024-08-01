9cead89b557880bc69bb5b6ddab1896a874a1c25
ducmint864 <nddminh2021@gmail.com>
2024-08-01 13:59:46 +0700

    Add class RpcRestManager (extension of HttpNodeManager)

---
+++ src/connections/chain-rpc/http/REST/RpcRestManager.ts
@@ -0,0 +1,58 @@
+import { HttpNodeManager } from "../HttpNodeManager";
+import { 
+	HttpNodeManagerError,
+	HttpNodeManagerErrorCode
+} from "../HttpNodeManagerError";
+import { chainRpcConfig } from "../../../../config";
+
+export class RpcRestManager extends HttpNodeManager {
+	public static readonly MIN_NODE_COUNT: number = chainRpcConfig.http.rpcRest.minNodes;
+	public static readonly MAX_NODE_COUNT: number = chainRpcConfig.http.rpcRest.maxNodes;
+
+	protected constructor() {
+		super();
+	}
+
+	public static init(): void {
+		if (RpcRestManager._instance) {
+			throw new Error("Concrete instance of RpcRestManager(HttpNodeManager) is already initialized");
+		}
+
+		this._instance = new RpcRestManager();
+	}
+
+	// Override
+	// TODO: check if the node is responsive before adding
+	public registerNode(url: string): void {
+		if (this.registeredNodeCount >= RpcRestManager.MAX_NODE_COUNT) {
+			throw new HttpNodeManagerError(
+				HttpNodeManagerErrorCode.ERR_MAX_NODES_REACHED,
+			);
+		}
+
+		if (!url) {
+			throw new HttpNodeManagerError(
+				HttpNodeManagerErrorCode.ERR_INVALID_URL,
+			);
+		}
+
+		if (this.isRegistered(url)) {
+			throw new HttpNodeManagerError(
+				HttpNodeManagerErrorCode.ERR_ALREADY_REGISTERED,
+			);
+		}
+
+		this._urls.add(url);	
+	}
+
+	// Override
+	public removeNode(url: string): void {
+		if (this.registeredNodeCount <= RpcRestManager.MIN_NODE_COUNT) {
+			throw new HttpNodeManagerError(
+				HttpNodeManagerErrorCode.ERR_MIN_NODES_REACHED,
+			);
+		}
+
+		this._urls.delete(url);
+	}
+}
