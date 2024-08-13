d88922de8dadcf081dbbe49f7c212a2fc02e558f
Agony Thorn <traminhtrong2604@gmail.com>
2024-08-13 19:19:45 +0700

    test: getDerivedAccount base test

--- test/unit_tests/crypto-helper.test.ts
+++ test/unit_tests/crypto-helper.test.ts
@@ -1,4 +1,4 @@
-import {makeHDPath} from "../../src/general/helpers/crypto-helper"
+import {makeHDPath, getDerivedAccount} from "../../src/general/helpers/crypto-helper"
 import {HdPath} from "@cosmjs/crypto"
 import { cryptoConfig } from "../../src/config"
 import { pathToString as hdPathToString, stringToPath as stringToHdPath } from "@cosmjs/crypto";
@@ -12,7 +12,7 @@ describe('makeHDPath', ()=> {
         const expectedPath: string = `m/44'/0'/19'/0/0`;
 
         expect(hdPathToString(HD_Path)).toBe(expectedPath);
-    })
+    });
 
     it('should be able to derive path with increment index', async ()=> {
         for(let index: number = 0; index < 3; index++){
@@ -21,7 +21,7 @@ describe('makeHDPath', ()=> {
 
             expect(hdPathToString(HD_Path)).toBe(expectedPath);
         }
-    })
+    });
 
     it('should return null when receive a negative index', () => {
         const accIndex: number = -1;
@@ -29,6 +29,17 @@ describe('makeHDPath', ()=> {
         const HD_Path: HdPath = makeHDPath(accIndex);
         
         expect(HD_Path).toBeNull();
-    })
-})
+    });
+});
 
+describe('getDerivedAcccount', () => {
+    it('should derive an account from a mnemonic and hdPath', async () => {
+        const mnemonic: string = "test test test test test test test test test test test junk";
+        const hdPath: HdPath = stringToHdPath("m/44'/0'/0'/0/0");
+
+        const derivedAccount = await getDerivedAccount(mnemonic, hdPath);
+        const expectedAccount_address: string = 'thasa1jesugmy5wq9jejy3zz0llsaynvndrauwnpywh3';
+
+        expect(derivedAccount.address).toBe(expectedAccount_address); 
+    });
+})
