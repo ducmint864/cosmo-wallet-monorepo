ebda3d6ab014a8d5fbffbbd9ceabdf421c5a161e
Agony Thorn <traminhtrong2604@gmail.com>
2024-08-19 19:30:56 +0700

    build: remove example test file

--- test/unit-tests/example.test.ts
+++
@@ -1,8 +0,0 @@
-import {describe, expect, test} from '@jest/globals';
-function sum(num1: number, num2: number): number{
-    return num1 + num2;
-}
-
-test('adds 1 + 2 to equal 3', () => {
-    expect(sum(1, 2)).toBe(3);
-  });
