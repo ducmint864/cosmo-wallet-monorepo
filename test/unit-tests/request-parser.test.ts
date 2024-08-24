4a5d3cbd586be3d8993de1d130dc6d939e8e4891
Agony Thorn <traminhtrong2604@gmail.com>
2024-08-24 20:31:20 +0700

    build test for getNumberArrayQueryParam

--- test/unit-tests/request-parser.test.ts
+++ test/unit-tests/request-parser.test.ts
@@ -137,3 +137,48 @@ describe('getBooleanQueryParam', () => {
         expect(result2).toBe(true);
     });
 });
+
+describe('getNumberArratQueryParam', () => {
+    it('should return a number array after parse each string from the string array', async () => {
+        // Arrange
+        const req = {
+            query: {
+                param: [
+                    "378", "hello", "world", "5tring"
+                ]
+            }
+        } as unknown as Request;
+
+        const paramName: string = "param";
+
+        // Act
+        const result = getNumberArrayQueryParam(req, paramName);
+
+        // Assert
+        expect(result).toBeDefined();
+        expect(result).toStrictEqual([378, 5])
+    });
+
+    it('should return a number after parse a string', async () => {
+        // Arrange
+        const req = {
+            query: {
+                param1: "71",
+                param2: "NaN123"
+            }
+        } as unknown as Request;
+
+        const paramName1: string = "param1";
+        const paramName2: string = "param2";
+
+        // Act
+        const result1 = getNumberArrayQueryParam(req, paramName1);
+        const result2 = getNumberArrayQueryParam(req, paramName2);
+
+        // Assert
+        expect(result1).toBeDefined();
+        expect(result2).toBeDefined();
+        expect(result1).toStrictEqual([71]);
+        expect(result2).toStrictEqual([]);
+    });
+});
