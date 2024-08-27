e5130890c8f581db5b18234a4abe627a23ed2296
Agony Thorn <traminhtrong2604@gmail.com>
2024-08-27 09:31:25 +0700

    test: register tests for successfully register a new user and fail to derived wallet for user

--- test/unit-tests/auth.test.ts
+++ test/unit-tests/auth.test.ts
@@ -4,6 +4,7 @@ import { HttpError } from "http-errors";
 import {errorHandler} from "../../src/errors/middlewares/error-handler"; 
 import {register} from '../../src/auth-module/controllers/auth'
 import { prisma } from "../../src/connections";
+import { cryptoConfig } from '../../src/config';
 
 /**
  * @dev error-handler mocking for register test
@@ -182,6 +183,9 @@ describe('register', () => {
     });
 
     describe('prisma mocking', () => {
+        /**
+         * @dev the wallet and user data has been tested in crypto-helper.test.ts
+         */
         beforeEach(() => {
             jest.clearAllMocks();
         })
@@ -264,6 +268,93 @@ describe('register', () => {
             expect(res.json).toHaveBeenCalledWith(
                 expect.objectContaining({ message: expect.any(String) })
             );
+        });
+
+        it('should register a new user if all requirements are met', async () => {
+            // Arrange
+            const req = ({
+                body: {
+                    email: 'coolguy82@example.com',
+                    username: 'coolguy82',
+                    password: 'P@ssw0rd'
+                    }
+            }) as Request;
+    
+            // Set up mock
+    
+            (prisma.user_accounts.create as jest.Mock)
+                .mockResolvedValue({
+                    data: {
+                        email: req.body.email,
+                        username: req.body.username,
+                        password: expect.any(String),
+                        crypto_mnemonic: expect.any(Buffer),
+                        crypto_iv: expect.any(Buffer),
+                        crypto_pbkdf2_salt: expect.any(Buffer)
+                    }
+                });
+                
+            (prisma.wallet_accounts.create as jest.Mock)
+                .mockResolvedValue({
+                    data: {
+                        address: expect.any(String),
+                        crypto_hd_path: expect.any(String),
+                        nickname: "Account 0",
+                        wallet_order: 1,
+                        is_main_wallet: true,
+                        user_account_id: expect.any(Number)
+                    }
+                });  
+
+            // Act
+    
+            await register(req, res, mockNext);
+        
+            // Assert
+
+            expect(res.status).toHaveBeenCalledWith(201);
+            expect(res.json).toHaveBeenCalledWith({
+                message: "Register successful"
+            });
+        });
+
+        it('should throw error 500 if it fail to register the user', async () => {
+            // Arrange
+            const req = ({
+                body: {
+                    email: 'coolguy82@example.com',
+                    username: 'coolguy82',
+                    password: 'P@ssw0rd'
+                    }
+            }) as Request;
+    
+            // Set up mock
+    
+            (prisma.user_accounts.create as jest.Mock)
+                .mockResolvedValue({
+                    data: {
+                        email: req.body.email,
+                        username: req.body.username,
+                        password: expect.any(String),
+                        crypto_mnemonic: expect.any(Buffer),
+                        crypto_iv: expect.any(Buffer),
+                        crypto_pbkdf2_salt: expect.any(Buffer)
+                    }
+                });
+            
+            (prisma.wallet_accounts.create as jest.Mock)
+                .mockResolvedValue(null);  
+
+            // Act
+    
+            await register(req, res, mockNext);
+        
+            // Assert
+
+            expect(res.status).toHaveBeenCalledWith(500);
+            expect(res.json).toHaveBeenCalledWith({
+                message: "Failed to create derived account"
+            });
         });
     });
 });
