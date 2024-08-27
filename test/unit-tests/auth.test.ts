c5e2d7edac9e602c3233ca39b40da22a45e3f4ee
Agony Thorn <traminhtrong2604@gmail.com>
2024-08-27 08:46:37 +0700

    test: unavaible username tests

--- test/unit-tests/auth.test.ts
+++ test/unit-tests/auth.test.ts
@@ -5,12 +5,27 @@ import {errorHandler} from "../../src/errors/middlewares/error-handler";
 import {register} from '../../src/auth-module/controllers/auth'
 import { prisma } from "../../src/connections";
 
-describe('register', () => {
-    /**
-     * @dev error-handler mocking for register test
-     */
-    jest.mock('../../src/errors/middlewares/error-handler');
+/**
+ * @dev error-handler mocking for register test
+ */
+jest.mock('../../src/errors/middlewares/error-handler');
 
+/**
+ * @dev prisma mock for database related test, preventing test to access actual database
+ */
+jest.mock("../../src/connections", () => ({
+    prisma: {
+      user_accounts: {
+        create: jest.fn(), 
+      },
+
+      wallet_accounts: {
+        create: jest.fn(),
+      }
+    },
+}));
+
+describe('register', () => {
     let res: Response;
     let mockNext: NextFunction;
 
@@ -163,6 +178,92 @@ describe('register', () => {
         expect(res.status).toHaveBeenCalledWith(400);
         expect(res.json).toHaveBeenCalledWith({
             message: expect.stringContaining("Invalid username:")
+        });
+    });
+
+    describe('prisma mocking', () => {
+        beforeEach(() => {
+            jest.clearAllMocks();
+        })
+        
+        it('should throw unavailable username database error to error handler', async () => {
+            // Arrange
+            const req = ({
+                body: {
+                    email: 'coolguy82@example.com',
+                    username: 'coolguy82',
+                    password: 'P@ssw0rd'
+                    }
+            }) as Request;
+    
+            // Set up mock
+    
+            (prisma.user_accounts.create as jest.Mock)
+                .mockRejectedValueOnce({
+                    code: 'P2002',
+                    meta: {
+                        target: ['username']
+                    }
+                });
+    
+            const mockHandleError = errorHandler as jest.Mock;
+            mockHandleError.mockImplementation((err, req, res, next) => {
+                res.status(err.statusCode).json({ message: err.message });
+            });
+    
+            // Act
+    
+            await register(req, res, mockNext);
+        
+            // Assert
+            expect(mockHandleError).toHaveBeenCalledWith(
+                expect.objectContaining({ code: 'P2002' }),
+                req, 
+                res,
+                mockNext
+            );
+        });
+
+        it('should handle the database error', async () => {
+            /**
+             * @dev we going to re-use the invalid username for this 
+             * @note fail test
+             * @note error-handler was able to receive the prisma error but can not handle it
+             * @todo design a method to convert prisma error to http error so that error handler can work appropriately
+             */
+            
+            const req = ({
+                body: {
+                    email: 'coolguy82@example.com',
+                    username: 'coolguy82',
+                    password: 'P@ssw0rd'
+                    }
+            }) as Request;
+        
+            // Set up mock
+        
+            (prisma.user_accounts.create as jest.Mock)
+                .mockRejectedValueOnce({
+                    code: 'P2002',
+                    meta: {
+                        target: ['username']
+                    }
+                });
+        
+            const mockHandleError = errorHandler as jest.Mock;
+            mockHandleError.mockImplementation((err, req, res, next) => {
+                res.status(err.statusCode).json({ message: err.message });
+            });
+        
+            // Act    
+        
+            await register(req, res, mockNext);
+        
+            // Assert
+            
+            expect(res.json).toHaveBeenCalledWith(
+                expect.objectContaining({ message: expect.any(String) })
+            );
         });
     });
 });
