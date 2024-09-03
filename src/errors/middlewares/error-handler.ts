1ac8f79016f1a2ccd489d06f1050d92208f49f41
ducmint864 <nddminh2021@gmail.com>
2024-09-03 20:06:16 +0700

    register new specific handlers regarding blockchain transactions

--- src/errors/middlewares/error-handler.ts
+++ src/errors/middlewares/error-handler.ts
@@ -2,16 +2,23 @@ import { PrismaClientKnownRequestError, PrismaClientUnknownRequestError } from "
 import { Request, Response, NextFunction } from "express";
 import { HttpError } from "http-errors";
 import { getErrorJSON } from "../helpers/error-message";
+import { BroadcastTxError, TimeoutError as TxTimeoutError } from "@cosmjs/stargate";
+
+// define type for specific error handler function
+type SpecificHandler = (...args: any[]) => Response;
+
+// import specific erorr handlers
 import { handlePrismaClientError } from "./prisma-error";
 import { handleHttpError } from "./http-error";
-
-type SpecificHandler = (...args: any[]) => Response;
+import { handleBroadcastTxError, handleTxTimeoutError } from "./tx-error";
 
 // map known types of error class to their specific handler func
 const specificHandlerMapping: Record<string, SpecificHandler> = {
 	[PrismaClientKnownRequestError.name]: handlePrismaClientError, // this is a func
 	[PrismaClientUnknownRequestError.name]: handlePrismaClientError, // this is a func
 	[HttpError.name]: handleHttpError,
+	[BroadcastTxError.name]: handleBroadcastTxError,
+	[TxTimeoutError.name]: handleTxTimeoutError,
 };
 
 function defaultSpecificHandler(err: Error, res: Response): Response {
@@ -31,7 +38,7 @@ function errorHandler(err: Error, req: Request, res: Response, next: NextFunctio
 	const specificHandler = specificHandlerMapping[errClassName]
 
 	if (specificHandler) {
-		return specificHandler(err, req, res);
+		return specificHandler(err, res);
 	}
 
 	return defaultSpecificHandler(err, res)
