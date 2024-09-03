d52d887b6e8c4838a43bec6465c67cd202a8465c
ducmint864 <nddminh2021@gmail.com>
2024-09-03 20:27:51 +0700

    export crucial functions of the transaction module via barrel file

--- src/transaction-module/index.ts
+++ src/transaction-module/index.ts
@@ -1,1 +1,19 @@
+export { makeRequestWithTimeout } from "./helpers/request";
 export { transactionRouter } from "./routes/";
+
+export { 
+	SaveTxPayload as PendingTxPayload,
+	createSaveTxPayload as createPendingTxPayload,
+} from "./types/SaveTxPayload"
+
+export {
+	saveTxToDb,
+} from "./helpers/save-tx";
+
+export {
+	getFeesFromTxResponse,
+} from "./helpers/tx-ressponse";
+
+export {
+	initTransactionModule,
+} from "./init-module";
