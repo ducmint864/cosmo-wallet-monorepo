c566ccd2194f80b36ddc87243813b3a14cb34607
ducmint864 <nddminh2021@gmail.com>
2024-09-05 21:59:21 +0700

    add multiple transactions test case

--- test/integration-tests/transaction/bank.test.ts
+++ test/integration-tests/transaction/bank.test.ts
@@ -2,6 +2,7 @@ import { describe, expect, test } from '@jest/globals';
 import request from "supertest"
 import { app } from "../../../src/index";
 import "dotenv/config";
+import { prisma } from '../../../src/connections';
 
 /**
  * @dev Provide a user account, which have at least 1 wallet account,
@@ -87,19 +88,19 @@ describe("sending coins API", () => {
 	let csrfToken: string;
 	let accessToken: string;
 	let refreshToken: string;
-	const testEmail: string = process.env["INTEGRATION_TEST_EMAIL"] as string;
-	const testPassword: string = process.env["INTEGRATION_TEST_PASSWORD"] as string;
-	const testAddress: string = process.env["INTEGRATION_TEST_ADDRESS"] as string;
+	const mainEmail: string = process.env["INTEGRATION_TEST_MAIN_EMAIL"] as string;
+	const mainPassword: string = process.env["INTEGRATION_TEST_MAIN_PASSWORD"] as string;
+	const mainAddress: string = process.env["INTEGRATION_TEST_MAIN_ADDRESS"] as string;
 	const bobAddress: string = process.env["INTEGRATION_TEST_BOB_ADDRESS"] as string;
 	const aliceAddress: string = process.env["INTEGRATION_TEST_ALICE_ADDRESS"] as string;
 
 	beforeAll(async () => {
-		expect(testEmail).not.toBeUndefined();
-		expect(testEmail).not.toBeFalsy();
-		expect(testPassword).not.toBeUndefined();
-		expect(testPassword).not.toBeFalsy();
-		expect(testAddress).not.toBeUndefined();
-		expect(testAddress).not.toBeFalsy();
+		expect(mainEmail).not.toBeUndefined();
+		expect(mainEmail).not.toBeFalsy();
+		expect(mainPassword).not.toBeUndefined();
+		expect(mainPassword).not.toBeFalsy();
+		expect(mainAddress).not.toBeUndefined();
+		expect(mainAddress).not.toBeFalsy();
 		expect(bobAddress).not.toBeUndefined();
 		expect(bobAddress).not.toBeFalsy();
 		expect(aliceAddress).not.toBeUndefined();
@@ -111,7 +112,7 @@ describe("sending coins API", () => {
 			csrfToken,
 			accessToken,
 			refreshToken
-		] = await loginAndReturnTokens(testEmail, testPassword);
+		] = await loginAndReturnTokens(mainEmail, mainPassword);
 
 		expect(csrfToken).not.toBeFalsy();
 		expect(accessToken).not.toBeUndefined();
@@ -140,8 +141,8 @@ describe("sending coins API", () => {
 				`refreshToken=${refreshToken}`,
 			])
 			.send({
-				password: testPassword,
-				fromAddress: testAddress, // my wallet's address
+				password: mainPassword,
+				fromAddress: mainAddress, // my wallet's address
 				toAddress: bobAddress,  // Bob's address
 				coin: {
 					denom: "stake",
@@ -164,8 +165,8 @@ describe("sending coins API", () => {
 				`refreshToken=${refreshToken}`,
 			])
 			.send({
-				password: testPassword,
-				fromAddress: testAddress, // my wallet's address
+				password: mainPassword,
+				fromAddress: mainAddress, // my wallet's address
 				toAddress: bobAddress,  // Bob's address
 				coin: {
 					denom: "stake",
@@ -177,7 +178,7 @@ describe("sending coins API", () => {
 	});
 
 	it("should respond with http status code 403 if provide invalid password", async () => {
-		const wrongPassword: string = reverseString(testPassword);
+		const wrongPassword: string = reverseString(mainPassword);
 
 		const res = await request(app)
 			.post("/api/transaction/bank/send-coin")
@@ -189,7 +190,7 @@ describe("sending coins API", () => {
 			])
 			.send({
 				password: wrongPassword,
-				fromAddress: testAddress, // my wallet's address
+				fromAddress: mainAddress, // my wallet's address
 				toAddress: bobAddress,  // Bob's address
 				coin: {
 					denom: "stake",
@@ -210,8 +211,8 @@ describe("sending coins API", () => {
 				`refreshToken=${refreshToken}`,
 			])
 			.send({
-				password: testPassword,
-				fromAddress: testAddress, // my wallet's address
+				password: mainPassword,
+				fromAddress: mainAddress, // my wallet's address
 				toAddress: bobAddress,  // Bob's address
 				coin: {
 					denom: "stake",
@@ -221,4 +222,188 @@ describe("sending coins API", () => {
 
 		expect(res.status).toBe(200);
 	});
+
+	it("handles multiple transactions at once well enough", async () => {
+		// register multiple test accounts
+		type Account = {
+			userAccountId: number,
+			email: string,
+			password: string,
+			mainWallet: {
+				walletAccountId: number,
+				address: string,
+			}
+			csrfToken: string,
+			accessToken: string,
+			refreshToken: string,
+		};
+
+		const stateMap = new Map<string, Partial<Account>>();
+		const txHashList: string[] = [];
+
+		try {
+			const emails: string[] = (process.env.INTEGRATION_TEST_MULTI_EMAILS as string)?.split(",");
+			const password: string = (process.env.INTEGRATION_TEST_MAIN_PASSWORD as string);
+			expect(emails).not.toBeUndefined();
+			expect(emails.length).not.toBeLessThan(1);
+			expect(password).not.toBeUndefined();
+			expect(password).not.toBeFalsy();
+
+			for (const email of emails) {
+				// register multiple accounts
+				const registerRes = await request(app)
+					.post("/api/auth/register")
+					.set("Accept", "application/json")
+					.send({
+						"email": email,
+						"password": password,
+					});
+				console.log(registerRes.body);
+
+				// save account info 
+				stateMap.set(email, {
+					"email": email,
+					"password": password,
+					"mainWallet": {
+						"address": registerRes.body["mainWallet"]["address"],
+						"walletAccountId": registerRes.body["mainWallet"]["walletAccountId"],
+					},
+					"userAccountId": registerRes.body["mainWallet"]["userAccountId"],
+				});
+				expect(registerRes.status).toBe(201);
+
+				// use main account to fund those test accounts
+				const sendRes = await request(app)
+					.post("/api/transaction/bank/send-coin")
+					.set("Accept", "application/json")
+					.set("x-csrf-token", csrfToken)
+					.set("Cookie", [
+						`accessToken=${accessToken}`,
+						`refreshToken=${refreshToken}`,
+					])
+					.send({
+						password: mainPassword,
+						fromAddress: mainAddress, // my wallet's address
+						toAddress: stateMap.get(email)?.mainWallet?.address,  // Bob's address
+						coin: {
+							denom: "stake",
+							amount: "100"
+						}
+					});
+				expect(sendRes.status).toBe(200);
+				txHashList.push(sendRes.body["txHash"]);
+
+				// then login to those accounts
+				const tokens = await loginAndReturnTokens(email, password);
+				stateMap.set(email, {
+					...stateMap.get(email),
+					"csrfToken": tokens[0],
+					"accessToken": tokens[1],
+					"refreshToken": tokens[2],
+				});
+				expect(stateMap.get(email)?.accessToken).not.toBeUndefined();
+				expect(stateMap.get(email)?.csrfToken).not.toBeUndefined();
+				expect(stateMap.get(email)?.refreshToken).not.toBeUndefined();
+			}
+
+			// act as those test accounts to send multiple transactions at once
+			const promises = Array.from(stateMap.values()).map((account) => {
+				return request(app)
+					.post("/api/transaction/bank/send-coin")
+					.set("Accept", "application/json")
+					.set("x-csrf-token", account.csrfToken!)
+					.set("Cookie", [
+						`accessToken=${account.accessToken}`,
+						`refreshToken=${account.refreshToken}`,
+					])
+					.send({
+						password: account.password,
+						fromAddress: account.mainWallet?.address,
+						toAddress: bobAddress,
+						coin: {
+							denom: "stake",
+							amount: "1"
+						}
+					});
+			});
+
+			const results = await Promise.all(promises);
+			results.forEach((res) => {
+				expect(res.status).toBe(200);
+				txHashList.push(res.body["txHash"]);
+			});
+		} catch (ignored) {
+			// expect(ignored).toThr
+			throw (ignored);
+		} finally {
+			// cleanup: delete all db records made in this test case
+
+			/**
+			 * THIS METHOD ALWAYS FAIL ON FK ERROR FOR SOME REASON
+			 */
+			// await prisma.transaction_fees.deleteMany({
+			// 	where: {
+			// 		transactions: {
+			// 			tx_hash: {
+			// 				in: txHashList
+			// 			}
+			// 		}
+			// 	}
+			// });
+			// await prisma.$transaction(async (prismaTran) => {
+			// 	await prismaTran.$queryRaw`delete from transaction_fees
+			// 	where tx_id in (
+			// 		select tx_id from transactions
+			// 		where tx_hash in (${Prisma.join(txHashList)})
+			// 	);
+			// `;
+			// 	console.log("TX HASH LIST: ", txHashList.join(", "));
+			// 	await prismaTran.transactions.deleteMany({
+			// 		where: {
+			// 			tx_hash: {
+			// 				in: txHashList
+			// 			}
+			// 		}
+			// 	});
+			// 	await prismaTran.wallet_accounts.deleteMany({
+			// 		where: {
+			// 			wallet_account_id: {
+			// 				in: Array.from(stateMap.values()).map(account => account.mainWallet!.walletAccountId)
+			// 			}
+			// 		}
+			// 	});
+			// 	await prismaTran.user_accounts.deleteMany({
+			// 		where: {
+			// 			user_account_id: {
+			// 				in: Array.from(stateMap.values()).map(account => account.userAccountId!)
+			// 			}
+			// 		}
+			// 	})
+			// });
+
+			await prisma.$transaction(async (prismaTrans) => {
+				await prismaTrans.transaction_fees.deleteMany({
+					where: {}
+				});
+				await prismaTrans.transactions.deleteMany({
+					where: {}
+				});
+				await prismaTrans.wallet_accounts.deleteMany({
+					where: {
+						wallet_account_id: {
+							in: Array.from(stateMap.values()).map((account) => account.mainWallet?.walletAccountId!)
+						}
+					}
+				})
+				await prismaTrans.user_accounts.deleteMany({
+					where: {
+						user_account_id: {
+							in: Array.from(stateMap.values()).map((account) => account.userAccountId!)
+						}
+					}
+				})
+			});
+		}
+
+	}, 70 * 1000);
 });
